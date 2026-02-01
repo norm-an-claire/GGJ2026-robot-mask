2bc0a566cdab64359ab80fc19b793d14d9b962bc
Andrew Eagle <a.eagle.andrew@gmail.com>
2026-01-31 20:26:00 -0700

    Got health_indicator to update sprite based on damage taken
    Has some wonkiness when the health "loops" back to 3 after death.

--- ui_scripts/health_indicator.gd
+++ ui_scripts/health_indicator.gd
@@ -1,15 +1,32 @@
 extends Node
 
+var sprites = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.player_damaged.connect(_on_player_damaged) 
+	_load_health_sprites()
+
+func _load_health_sprites():
+	sprites = {
+		"full_health": load("res://assets/ui_assets/full_health_base.png"),
+		"medium_health": load("res://assets/ui_assets/medium_health_base.png"),
+		"low_health": load("res://assets/ui_assets/low_health_base.png")
+	}
 	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func _on_player_damaged(currHitPoints: int):
-	print("Hello there")
-	
+	print("in _on_player_damaged")
+	# Add code to swap the sprite for the health based on the incoming
+	# current amount
+	match currHitPoints:
+		3:
+			# Set sprite to full_health_base
+			self.texture = sprites["full_health"]
+		2:
+			self.texture = sprites["medium_health"]
+		1:
+			self.texture = sprites["low_health"]
+		0: 
+			self.texture = sprites["low_health"]
+		_:
+			pass
